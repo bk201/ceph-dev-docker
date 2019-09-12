04629840bc2ce38c214f216efebfdb1291bd5222
Kiefer Chang <kiefer.chang@suse.com>
2019-09-12 10:39:01 +0800

    proxy to backend in Rook deployment

--- shared/bin/npm-start.sh
+++ shared/bin/npm-start.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-setup-proxy.sh
+if [ -n "$PROXY_K8S" ]; then
+    setup-proxy-k8s.sh
+else
+    setup-proxy.sh
+fi
 
 cd /ceph/src/pybind/mgr/dashboard/frontend
 source /ceph/build/src/pybind/mgr/dashboard/node-env/bin/activate
